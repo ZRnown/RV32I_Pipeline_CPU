cd4963591c45507da006b7c81bd4408e039fdb09
cpython333 <3479821557@qq.com>
2025-03-16 18:59:35 +0800

    完成补全b型分支和J型跳转指令

--- sim/testcases/cpu_top_tb.v
+++ sim/testcases/cpu_top_tb.v
@@ -15,7 +15,7 @@ module tb;
   end
   initial begin
     $readmemh(
-        "E:\\Files\\Electron\\FPGA\\RV32I_Pipeline_CPU\\sim\\testcases\\inst_test\\rv32ui-p-beq.txt",
+        "E:\\shiyanshi\\project\\DevADemo\\RV32I_Pipeline_CPU\\sim\\testcases\\inst_test\\rv32ui-p-jalr.txt",
         tb.u_cpu_top_soc.u_rom.ROM);
   end
   initial begin
