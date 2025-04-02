e57ef46860a37c1e3d5cd29837902616ba68f3d4
ZRnown <2902414706@qq.com>
2025-04-02 17:27:26 +0800

    aa

--- fpga/RISCV/RISCV.runs/synth_1/cpu_top_soc.tcl
+++ fpga/RISCV/RISCV.runs/synth_1/cpu_top_soc.tcl
@@ -115,6 +115,8 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
 set_param ips.enableIPCacheLiteLoad 1
+
+read_checkpoint -auto_incremental -incremental E:/Files/Electron/FPGA/RV32I_Pipeline_CPU/fpga/RISCV/RISCV.srcs/utils_1/imports/synth_1/cpu_top_soc.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
